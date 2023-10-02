b77201fa58dfbeb8cec01211fa1ba3f5f308e4f0
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-10-02 19:14:33 +0500

    Add project files.

---
+++ ProductForCreatingDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpCRUD
+{
+    public class ProductForCreationDto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public long? CategoryId { get; set; }
+
+        //public IFormFile? File { get; set; }
+    }
+}
